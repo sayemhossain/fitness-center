6a8040d41c0279de58179fd4f33e36a7cb6e1d50
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-17 02:53:27 +0600

    router setup done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Shared/Header/Header";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import Blogs from "./components/Pages/Blogs/Blogs";
+import About from "./components/Pages/About/About";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Pages/Blogs/Blogs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Blogs.css";
+
+const Blogs = () => {
+  return (
+    <div>
+      <h4>this is blogs section</h4>
+    </div>
+  );
+};
+
+export default Blogs;
