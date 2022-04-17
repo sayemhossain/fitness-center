b8b1978809d8cc8e6613977933d1a07e51dab08d
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-17 21:43:16 +0600

    services section done

--- src/components/Pages/Home/Service/Service.js
+++ src/components/Pages/Home/Service/Service.js
@@ -1,23 +1,30 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import "./Service.css";
+import { Link } from "react-router-dom";
 
 const Service = (props) => {
   const { name, price, img, description } = props.service;
 
   return (
     <div className="mx-auto my-3">
-      <div className="card rounded p-2" style={{ width: "18rem" }}>
+      <div className="card rounded p-2 text-center" style={{ width: "18rem" }}>
         <img src={img} className="card-img-top" alt="..." />
-        <p className="text-center mb-0 mt-2 text-danger fw-bold">{price}</p>
+        <h3 className="text-center mb-0 mt-2 card-price ">{price}</h3>
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
-          <p className="card-text">
+          <p className="card-text mb-4">
             {description.length > 150
               ? description.slice(0, 150) + "..."
               : description}
           </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <Link
+            to="/"
+            className="card-btn rounded-pill px-4 py-2 text-white text-uppercase"
+          >
+            <FontAwesomeIcon className="me-1" icon={faCartShopping} /> purchase
+          </Link>
         </div>
       </div>
     </div>
--- src/components/Pages/Home/Services/Services.js
+++ src/components/Pages/Home/Services/Services.js
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
+import useServices from "../../../../hooks/useServices";
 import Service from "../Service/Service";
 import "./Services.css";
 
 const Services = () => {
-  const [services, setServices] = useState([]);
-  useEffect(() => {
-    fetch("data.json")
-      .then((res) => res.json())
-      .then((data) => setServices(data));
-  }, []);
+  const [services, setServices] = useServices();
   return (
     <div className="container my-5">
       <div>
@@ -18,7 +14,7 @@ const Services = () => {
         </h2>
         <hr className="w-50 mx-auto " />
       </div>
-      <div className="service-container container my-4">
+      <div className="service-container container my-5">
         {services.map((service) => (
           <Service key={service.id} service={service}></Service>
         ))}
